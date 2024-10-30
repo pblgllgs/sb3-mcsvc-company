98943bc6537470c663feff9e3341c20e875aeb31
pblgllgs <pbl.gllgs@gmail.com>
2024-10-30 10:44:06 -0300

    initial commit

---
+++ src/main/java/com/pblgllgs/sb3mcsvccompany/Sb3McsvcCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.sb3mcsvccompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3McsvcCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3McsvcCompanyApplication.class, args);
+    }
+
+}
