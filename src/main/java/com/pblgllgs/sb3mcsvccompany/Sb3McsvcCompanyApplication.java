ac85380c1c28e7d1d58577d065a07d8bb29bb5a2
pblgllgs <pbl.gllgs@gmail.com>
2024-11-05 13:05:12 -0300

    add eureka discovery service

--- src/main/java/com/pblgllgs/sb3mcsvccompany/Sb3McsvcCompanyApplication.java
+++ src/main/java/com/pblgllgs/sb3mcsvccompany/Sb3McsvcCompanyApplication.java
@@ -2,8 +2,10 @@ package com.pblgllgs.sb3mcsvccompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Sb3McsvcCompanyApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/pblgllgs/sb3mcsvccompany/company/CompanyService.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.sb3mcsvccompany.company;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompanyService {
+
+    List<Company> findAll();
+    void createCompany(Company company);
+    Optional<Company> findCompanyById(Long id);
+    boolean deleteCompany(Long id);
+    boolean updateCompany(Long id, Company company);
+}
