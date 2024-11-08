a89b60ad8ef627318592930470a5629ed7a182d9
pblgllgs <pbl.gllgs@gmail.com>
2024-11-08 12:39:56 -0300

    add config client properties

--- src/main/java/com/pblgllgs/sb3mcsvccompany/company/CompanyController.java
+++ src/main/java/com/pblgllgs/sb3mcsvccompany/company/CompanyController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/company")
+@RequestMapping("/companies")
 public class CompanyController {
 
     private final CompanyService companyService;
